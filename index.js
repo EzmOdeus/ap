976d920a06644295744922a18ec614fb77448c2e
EzmOdeus <shafrax500@gmail.com>
2023-10-15 00:40:39 +0300

    x

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name:"spiders"
+        }
+        ,
+        {
+            id: 2,
+            name:"seyam"
+        }
+        ,
+        {
+            id: 3,
+            name:"ahmed"
+        }
+    ])
+    
+})
+app.get('/form', (req, res) => {
+    res.send(`welcome ${req.query.fname}`);
+    console.log();
+})
+app.listen(5000, () => {
+    console.log('server OK!')
+})
+
+// // const os = require('os')
+// // console.log(`arch is : ${os.arch}`);
+// // console.log(`host name is : ${os.hostname}`);
+// // console.log(`freespace is : ${os.freemem}`);
+// // console.log(`platform is : ${os.platform}`);
+// // console.log(`type is : ${os.type}`);
+// // console.log(`version is : ${os.version}`);
+
+// // ===========
+// const fs = require('fs')
+// console.log(fs.readFileSync('hi.txt', 'utf-8'));
+// // fs.unlink('./hello.txt',function () {
+    
+// // })
+// fs.mkdir('./newdir', (err) => {
+//     if (err) {
+//         console.log(`error`);
+//     }
+//     else {
+//         process.chdir('./newdir')
+//         fs.writeFile('./seyam.txt', 'semo', () => {
+            
+//         })
+//     }
+// })
